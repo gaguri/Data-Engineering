26e56628fe605593620fd8ec98f905dbc5921f2f
Svetlana Levina <gurigaguri@yandex.com>
2024-11-17 00:49:47 +0500

    Update task4.py

--- lab1/task4.py
+++ lab1/task4.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Чтение файла
-df = pd.read_csv('/root/lab1/files/fourth_task.txt')
+df = pd.read_csv('lab1/files/fourth_task.txt')
 
 # Удаление колонки
 df_dropped = df.drop('status', axis=1)
@@ -19,10 +19,10 @@ min_value = df['rating'].min()
 df_filtered = df[df['price'] < 277]
 
 # Сохранение модифицированного csv-файла
-df_filtered.to_csv('/root/lab1/files/results/task4_1.csv', index=False, header=True)
+df_filtered.to_csv('lab1/files/results/task4_1.csv', index=False, header=True)
 
 # Сохранение результатов вычислений
-with open("/root/lab1/files/results/task4_2.txt", "w") as file:
+with open("lab1/files/results/task4_2.txt", "w") as file:
             file.write(f"Среднее арифметическое по столбцу rating: {mean_value}\n\
 Максимальное значение по столбцу quantity: {max_value}\n\
 Минимальное значение по стобцу rating: {min_value}")
